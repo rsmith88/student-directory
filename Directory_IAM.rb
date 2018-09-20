377a3cd07b4c8c3d2b400ea9086b63556f36cc30
Raylene <smith.raylene@gmail.com>
2018-09-20 20:58:00 +0100

    Modified interactive menu to allow saving to file.

--- Directory_IAM.rb
+++ Directory_IAM.rb
@@ -1,5 +1,17 @@
 @students = [] # an empty array accessible by all files
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of save_students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -13,6 +25,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate.
     else
@@ -23,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # because we'll be adding more items
 end
 
