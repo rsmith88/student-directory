0e4391cdcee64072c437d8e97164974ad8eaa5e9
Raylene <smith.raylene@gmail.com>
2018-09-20 17:32:23 +0100

    Modified for Exercise 10

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
 	#create an empty array
 	students =[]
 	#get the first name
-	name = gets.chomp
+	name = gets.delete "\n"
 	#while the name is not empty, repeat the code
 	while !name.empty? do 
 		#add the student hash to the array
