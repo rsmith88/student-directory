e7411759daddfdffaf84fa104d822b094b1beea5
Raylene <smith.raylene@gmail.com>
2018-09-20 22:49:20 +0100

    Added command line function

--- Directory_IAM.rb
+++ Directory_IAM.rb
@@ -1,7 +1,19 @@
 @students = [] # an empty array accessible by all files
 
-def load_students
-  file = File.open("students.csv", "r")
+def try_load_students
+  filename = ARGV.first # first argument from the command command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry #{filename} doesn't exist."
+    exit #quit the program
+  end
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
       @students << {name: name, cohort: cohort.to_sym}
@@ -24,7 +36,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -56,14 +68,14 @@ end
 def input_students
   puts "Please enter the names of the students"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -90,4 +102,5 @@ def show_students
   print_footer
 end
 
+try_load_students
 interactive_menu
