6452d39dd8d2a46d8c7988809027e2ef6162be5f
Raylene <smith.raylene@gmail.com>
2018-09-20 21:45:28 +0100

    Modified interactive menu to allow loading file

--- Directory_IAM.rb
+++ Directory_IAM.rb
@@ -1,5 +1,14 @@
 @students = [] # an empty array accessible by all files
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+      @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+end
+
 def save_students
   # open the file for writing
   file = File.open("students.csv", "w")
@@ -27,6 +36,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit # this will cause the program to terminate.
     else
@@ -38,6 +49,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # because we'll be adding more items
 end
 
