3a3c404833f62a0bb515433f28943990e6d98f34
Raylene <smith.raylene@gmail.com>
2018-09-20 10:06:30 +0100

    Modified for Excercise 2

--- directory.rb
+++ directory.rb
@@ -25,7 +25,9 @@ end
 
 def print(students)
 	students.each_with_index do |student, index|
-		puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+		if student[:name].split("").first == "T"
+			puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+		end
 	end
 end 
 
