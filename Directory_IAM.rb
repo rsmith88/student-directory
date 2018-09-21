2c38cc0cc332aae7e018c383cee178a0efd419f1
Raylene <smith.raylene@gmail.com>
2018-09-21 20:08:57 +0100

    Modified interactive menu file for Exercises 1-4

--- Directory_IAM.rb
+++ Directory_IAM.rb
@@ -2,29 +2,40 @@
 
 def try_load_students
   filename = ARGV.first # first argument from the command command line
-  return if filename.nil? #get out of the method if it isn't given
-  if File.exists?(filename) #if it exists
+  if filename.nil?
+    load_students
+    puts "Loaded #{@students.count} from students.csv"
+  else File.exists?(filename) #if it exists
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry #{filename} doesn't exist."
-    exit #quit the program
+    "Loaded #{@students.count} from #{filename}"
   end
 end
 
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-      @students << {name: name, cohort: cohort.to_sym}
+      name, cohort = line.chomp.split(',')
+      add_students(name, cohort)
     end
     file.close
 end
 
+def add_students(name, cohort = "november")
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    add_students(name)
+    print "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+end
+
 def save_students
-  # open the file for writing
   file = File.open("students.csv", "w")
-  # iterate over the array of save_students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -37,21 +48,17 @@ def interactive_menu
   loop do
     print_menu
     process(STDIN.gets.chomp)
+    puts "Task successfully completed."
   end
 end
 
 def process(selection)
   case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit # this will cause the program to terminate.
+    when "1" then input_students
+    when "2" then show_students
+    when "3" then save_students
+    when "4" then load_students
+    when "9" then exit # this will cause the program to terminate.
     else
       puts "I don't know what you meant, try again"
   end
@@ -63,20 +70,6 @@ def print_menu
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit" # because we'll be adding more items
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  # get the first name
-  name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = STDIN.gets.chomp
-  end
 end
 
 def print_header
