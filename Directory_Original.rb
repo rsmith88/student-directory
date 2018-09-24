95aad615ea1e4fd85ce018d7209d1a44e7db4bde
Raylene <smith.raylene@gmail.com>
2018-09-24 17:17:54 +0100

    Refactored section of original directory code

--- Directory_Original.rb
+++ Directory_Original.rb
@@ -1,31 +1,21 @@
+@students = Array.new
+
 def input_students
 	puts "Please enter the name of the first student. To finish hit return."
-	#puts "To finish just hit return twice"
-	#create an empty array
-	students =[]
-	#get the first name
 	name = gets.delete "\n"
-	#while the name is not empty, repeat the code
 	while !name.empty? do 
-		#add the student hash to the array
-		puts "What cohort will #{name} be joining?"
-		cohort = gets.chomp
-		cohort = "TBD" if cohort == ""
-		puts "How old is #{name}?"
-		age = gets.chomp
-		age = "TBD" if age == ""
-		puts "What country is #{name} from?"
-		country = gets.chomp
-		country = "TBD" if country == ""
+		cohort = input_info(name, "cohort")
+		age = input_info(name, "age")
+		country = input_info(name, "country")
 		puts "You have entered:   Name: #{name}  Cohort: #{cohort}  Age: #{age}  Country: #{country}"
 		puts "Type 'Yes' to confirm. To start again, type 'No.' To confirm & exit, hit return"
 			confirm = gets.chomp
 		loop do 
 			if (confirm == "yes") or (confirm == "Yes")
-				students << {name: name.to_sym, cohort: cohort.to_sym, country: country.to_sym, age: age.to_sym}
-				puts "Now we have #{students.count} students" if students.count > 1
-				puts "Now we have #{students.count} students" if students.count == 1
-				puts "No students currently enrolled" if students.count == 0 
+				@students << {name: name.to_sym, cohort: cohort.to_sym, country: country.to_sym, age: age.to_sym}
+				puts "Now we have #{@students.count} students" if @students.count > 1
+				puts "Now we have #{@students.count} students" if @students.count == 1
+				puts "No students currently enrolled" if @students.count == 0 
 				#get another name from the user 
 				puts "Type in the name of the next student, or return to finish."
 				name = gets.chomp
@@ -35,7 +25,7 @@ def input_students
 				name = gets.chomp
 				break
 			elsif confirm == ""
-				students << {name: name.to_sym, cohort: cohort.to_sym, country: country.to_sym, age: age.to_sym}
+				@students << {name: name.to_sym, cohort: cohort.to_sym, country: country.to_sym, age: age.to_sym}
 				name = ""
 				break
 			else
@@ -45,20 +35,26 @@ def input_students
 		end
 	end
 	#return the array of students
-	students 
+	@students 
 end 
 
+def input_info(name, fact)
+	puts "Please enter the #{fact.upcase} for #{name}:"
+		fact = gets.chomp
+		fact = "TBD" if fact == ""
+		return fact 
+end
 
 def print_header
-	puts "The students by cohort at Makers Academy".center(50)
+	puts "The Students by Cohort at Makers Academy".center(50)
 	puts "-------------".center(50)
 end
 
-def print(students)
-	if students.empty? != true
+def print_students
+	if @students.empty? != true
 		cohort = ""
 		counter = 0
-		students_by_cohort = students.sort_by { |student| student[:cohort]} 
+		students_by_cohort = @students.sort_by { |student| student[:cohort]} 
 		students_by_cohort.each_with_index do |student, index|
 			if cohort != "#{student[:cohort]}"
 				puts "\n#{student[:cohort]} Cohort"
@@ -71,14 +67,13 @@ def print(students)
 	end
 end 
 
-def print_footer(students)
-	puts "\nCurrently, there are no students enrolled." if students.count == 0
-	puts "\nOverall, we have #{students.count} great student!" if students.count == 1
-	puts "\nOverall, we have #{students.count} great students!" if students.count > 1
+def print_footer
+	puts "\nCurrently, there are no students enrolled." if @students.count == 0
+	puts "\nOverall, we have #{@students.count} great student!" if @students.count == 1
+	puts "\nOverall, we have #{@students.count} great students!" if @students.count > 1
 end
 
-#nothing happens until we call the methods
-students = input_students
+input_students
 print_header
-print(students)
-print_footer(students)
+print_students
+print_footer
